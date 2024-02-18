924d5f4185f6c851a382446238869a5ab7a873cf
Kashfi Fahim <kashfi.fahim@gmail.com>
2024-02-18 16:57:12 -0500

    Session 1

--- script.js
+++ script.js
@@ -8,4 +8,9 @@ let isError = false;
 
 function cleanInputString(str) {
     const regex = /[+-\s]/g;
+    return str.replace(regex, '');
+}
+
+function isInvalidInput(str) {
+    const regex = /\d+e\d+/i;
 }
